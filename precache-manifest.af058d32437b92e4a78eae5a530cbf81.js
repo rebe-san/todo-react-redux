1afcac3939b75b9b8db6db997ba4881954b5237f
Rebeca Chiñas <chi.san.rebeca@gmail.com>
2020-05-07 13:48:06 -0500

    Updates

---
+++ precache-manifest.af058d32437b92e4a78eae5a530cbf81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4ae51563b36c2ce1d1aa691cdbde369",
+    "url": "/rebe-san/TODO-react-redux/index.html"
+  },
+  {
+    "revision": "136a5ebf27e9bbcd3380",
+    "url": "/rebe-san/TODO-react-redux/static/css/main.89b6b862.chunk.css"
+  },
+  {
+    "revision": "3323edf795eae2268030",
+    "url": "/rebe-san/TODO-react-redux/static/js/2.93c3803f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rebe-san/TODO-react-redux/static/js/2.93c3803f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "136a5ebf27e9bbcd3380",
+    "url": "/rebe-san/TODO-react-redux/static/js/main.fd08da3b.chunk.js"
+  },
+  {
+    "revision": "ff7e98493727121cdd2e",
+    "url": "/rebe-san/TODO-react-redux/static/js/runtime-main.2e4e1fd8.js"
+  }
+]);
